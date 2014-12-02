2f1ce0d13d6888323744d5e33a3cd13d5b27acbd
KennySing <hello@kennysing.com>
2014-12-01 23:22:43 -0800

    added portfolio icon to shiftnav

--- header.php
+++ header.php
@@ -178,6 +178,7 @@
 
 					<?php do_action( 'et_header_top' ); ?>
 				</div> <!-- #et-top-navigation -->
+				<a href="/kennysing/#portfolio"><div id="portfolio-icon"></div></a>
 			</div> <!-- .container -->
 		</header> <!-- #main-header -->
 
